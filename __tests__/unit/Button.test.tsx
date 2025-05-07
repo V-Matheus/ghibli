2eaffdd4c3dab0aa9cc6dd1153535bc999de1f6c
Victor Matheus <victormatheus507@gmail.com>
2025-05-07 18:36:47 -0300

    test: Button

---
+++ __tests__/unit/Button.test.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/Button';
+import { render } from '@testing-library/react';
+
+describe('Component: Button', () => {
+  it('should render a button', () => {
+    const screen = render(<Button>Button Content</Button>);
+
+    expect(
+      screen.getByRole('button', { name: 'Button Content' }),
+    ).toBeInTheDocument();
+  });
+});
